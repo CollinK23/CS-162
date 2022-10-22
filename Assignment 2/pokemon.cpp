22ed999f4b0cea889f684fc27bb3a5ba1d5fa34a
CollinK23 <kimballcollin@gmail.com>
2022-10-22 01:37:34 -0700

    error handling

--- Assignment 2/pokemon.cpp
+++ Assignment 2/pokemon.cpp
@@ -63,7 +63,6 @@ void txtOutput(int j, Pokemon* selectPokemon, int numOfPokemon){
 
     fout.open(fileName.c_str());
 
-
     fout << "Dex Number: " << selectPokemon[j].dex_num << endl;
     fout << "Name: " << selectPokemon[j].name << endl;
     fout << "Type: " << selectPokemon[j].type << endl;
@@ -72,6 +71,8 @@ void txtOutput(int j, Pokemon* selectPokemon, int numOfPokemon){
     for (int i = 0; i < selectPokemon[j].num_moves; i++){
         fout << "Move" << i + 1 << ": " << selectPokemon[j].moves[i] << endl;
     }
+
+    fout << endl << endl;
 }
 
 void terminalOutput(int j, Pokemon* selectPokemon, int numOfPokemon){
@@ -83,51 +84,142 @@ void terminalOutput(int j, Pokemon* selectPokemon, int numOfPokemon){
     for (int i = 0; i < selectPokemon[j].num_moves; i++){
         cout << "Move" << i + 1 << ": " << selectPokemon[j].moves[i] << endl;
     }
+
+    cout << endl << endl;
+}
+
+
+
+//----------------------------------------------- SEARCH METHODS --------------------------------------------
+
+int outputSelector(){
+    int selectOutput;
+    while (true){
+        cout << "(1.) Output to .txt File" << endl;
+        cout << "(2.) Output to Terminal" << endl;
+        cin >> selectOutput;
+        if (selectOutput == 1 or selectOutput == 2){
+            break;
+        }else {
+            cout << "Invalid Output Format" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<int>::max(), '\n');
+        }
+    }
+    return selectOutput;
 }
 
 int searchByDex(Pokemon* selectPokemon, int dexNum, int numOfPokemon){
-    int selectOutput;
+    bool found = false;
     for (int j = 0; j < numOfPokemon; j++){
         if (selectPokemon[j].dex_num == dexNum){
-            while (true){
-                cout << "(1.) Output to .txt File" << endl;
-                cout << "(2.) Output to Terminal" << endl;
-                cin >> selectOutput;
-
-                if (selectOutput == 1){
-                    txtOutput(j, selectPokemon, numOfPokemon);
-                    return 0;
-                }else if (selectOutput == 2){
-                    terminalOutput(j, selectPokemon, numOfPokemon);
-                    return 0;
-                }else if (selectOutput != 1 or selectOutput != 2){
-                    searchPrompt(numOfPokemon, selectPokemon);
-                    return 0;
-                }
+            found = true;
+            int selectOutput = outputSelector();
+            if (selectOutput == 1){
+                txtOutput(j, selectPokemon, numOfPokemon);
+                searchPrompt(numOfPokemon, selectPokemon);
+            }else if (selectOutput == 2){
+                terminalOutput(j, selectPokemon, numOfPokemon);
+                searchPrompt(numOfPokemon, selectPokemon);
+            }else if (selectOutput != 1 or selectOutput != 2){
+                searchPrompt(numOfPokemon, selectPokemon);
             }
         }
     }
-    cout << "Pokemon Does Not Exist In the Dex" << endl;
+
+    if (!found){
+        cout << "Pokemon Does Not Exist In the Dex" << endl;
+    }
     searchPrompt(numOfPokemon, selectPokemon);
 }
+
+int searchByName(Pokemon* selectPokemon, string pokeName, int numOfPokemon){
+    bool found = false;
+    for (int j = 0; j < numOfPokemon; j++){
+        if (selectPokemon[j].name == pokeName){
+            found = true;
+            int selectOutput = outputSelector();
+            if (selectOutput == 1){
+                txtOutput(j, selectPokemon, numOfPokemon);
+                searchPrompt(numOfPokemon, selectPokemon);
+            }else if (selectOutput == 2){
+                terminalOutput(j, selectPokemon, numOfPokemon);
+                searchPrompt(numOfPokemon, selectPokemon);
+            }else if (selectOutput != 1 or selectOutput != 2){
+                searchPrompt(numOfPokemon, selectPokemon);
+            }
+        }
+    }
+
+    if (!found){
+        cout << "Pokemon Does Not Exist In the Dex" << endl;
+    }
+    searchPrompt(numOfPokemon, selectPokemon);
+}
+
+//FIx this function
+
+void searchByType(Pokemon* selectPokemon, string pokeType, int numOfPokemon){
+    bool found = false;
+    for (int j = 0; j < numOfPokemon; j++){
+        if (selectPokemon[j].type == pokeType){
+            found = true;
+            break;
+        }
+    }
+    if (!found){
+        cout << "Pokemon With This Type Does Not Exist In the Dex" << endl;
+        searchPrompt(numOfPokemon, selectPokemon);
+        return;
+    }
+    int selectOutput = outputSelector();
+    ofstream fout;
+    string fileName;
+
+    if (selectOutput == 1){
+        cout << "Select The File You Want To Output To: " << endl;
+        cin >> fileName;
+
+        fout.open(fileName.c_str());
+    }
+
+    for (int j = 0; j < numOfPokemon; j++){
+        if (selectPokemon[j].type == pokeType){
+            if (selectOutput == 1){
+                fout << "Dex Number: " << selectPokemon[j].dex_num << endl;
+                fout << "Name: " << selectPokemon[j].name << endl;
+                fout << "Type: " << selectPokemon[j].type << endl;
+                fout << "Number of Moves: " << selectPokemon[j].num_moves << endl;
+                
+                for (int i = 0; i < selectPokemon[j].num_moves; i++){
+                    fout << "Move" << i + 1 << ": " << selectPokemon[j].moves[i] << endl;
+                }
+
+                fout << endl << endl;
+
+            }else if (selectOutput == 2){
+                terminalOutput(j, selectPokemon, numOfPokemon);
+            }
+        }
+    }
+    searchPrompt(numOfPokemon, selectPokemon);
+}
+
 
 int selectSearchMethod(){
     int searchMethod;
 
     cout << "Choose a Way to Display the Pokedex Information"<< endl;
     cout << "1. Search By Dex Number" << endl << "2. Search By Name" << endl;
-    cout << "3. Search By Type" << endl << "4. Add New Pokemon" << endl;
+    cout << "3. Search By Type" << endl << "4. Add New Pokemon" << endl << "5. Exit Program" << endl << endl;
 
     cin >> searchMethod;
 
     return searchMethod;
 }
 
-void searchPrompt(int numOfPokemon, Pokemon* selectPokemon){
-    int searchMethod = selectSearchMethod();
-
+int dexCheck(){
     cout << "Enter the Dex Number of the Pokemon: " << endl;
-    if (searchMethod == 1){
         int dexNum;
         while (true){
             cin >> dexNum;
@@ -140,24 +232,70 @@ void searchPrompt(int numOfPokemon, Pokemon* selectPokemon){
             }
             else break;
         }
+    return dexNum;
+}
+
+string nameCheck(){
+    cout << "Enter the Name of the Pokemon: " << endl;
+        string pokeName;
+        while (true){
+            cin >> pokeName;
+
+            if (!cin){
+                cout << "Invalid Name. Enter again: " << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<int>::max(), '\n');
+                continue;
+            }
+            else break;
+        }
+    return pokeName;
+}
+
+string typeCheck(){
+    cout << "Enter the Type of the Pokemon: " << endl;
+        string pokeType;
+        while (true){
+            cin >> pokeType;
+
+            if (!cin){
+                cout << "Invalid Type. Enter again: " << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<int>::max(), '\n');
+                continue;
+            }
+            else break;
+        }
+    return pokeType;
+}
+
+void searchPrompt(int numOfPokemon, Pokemon* selectPokemon){
+    int searchMethod = selectSearchMethod();
+
+    if (searchMethod == 1){
+        int dexNum = dexCheck();
         searchByDex(selectPokemon, dexNum, numOfPokemon);
+        return;
         
     }else if (searchMethod == 2){
-        string pokeName;
-        cout << "Enter the Name of the Pokemon: " << endl;
-        cin >> pokeName;
-        cout << "Function 2 Goes Here " << pokeName << endl;
+        string pokeName = nameCheck();
+        searchByName(selectPokemon, pokeName, numOfPokemon);
+        return;
 
     }else if (searchMethod == 3){
-        string pokeType;
-        cout << "Enter the Type of the Pokemon: " <<endl;
-        cin >> pokeType;
-        cout << "Function 3 Goes Here " << pokeType << endl;
+        string pokeType = typeCheck();
+        searchByType(selectPokemon, pokeType, numOfPokemon);
+        return;
 
     }else if (searchMethod == 4){
         cout << "Function 4 Goes Here" << endl;
+        
+
+    }else if (searchMethod == 5){
+        return;
 
     }else{
         searchPrompt(numOfPokemon, selectPokemon);
+        return;
     }
 }
